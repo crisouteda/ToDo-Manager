975916a05c2b6dd1db2e5f8f89150bd3eb8a3df3
cristina <cristina.outeda.rua@gmail.com>
2022-01-22 15:16:47 +0100

    formated

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,35 @@
 <script>
-	import Dashboard from "./components/Dashboard.svelte" 
-	import Modal from "./components/Modal.svelte";
-	let titleList = ["default"];
+  import Dashboard from "./components/Dashboard.svelte";
+  import Modal from "./components/Modal.svelte";
+  let titleList = ["default"];
 </script>
 
 <main>
-	<!-- <Modal/> -->
-	<Dashboard {titleList} />
-	<button on:click={() => titleList=[...titleList,"default"]}>+</button>
+  <!-- <Modal/> -->
+
+  <Dashboard {titleList} />
+  <button on:click={() => (titleList = [...titleList, "default"])}>+</button>
 </main>
 
 <style>
-	main {
-		width: 100%;
-		height: 100%;
-		display: flex;
-		background: #e2e8f0;
-	}
+  main {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    background: #e2e8f0;
+  }
 
-	button {
-		background: #0BDA51;
-		border: 0px;
-		height: 30px;
-		width: 30px;
-		border-radius: 50px;
-		margin: 35px 5px 0px ;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		font-size: 28px;
-		color: #fff;
-	}
+  button {
+    background: #0bda51;
+    border: 0px;
+    height: 30px;
+    width: 30px;
+    border-radius: 50px;
+    margin: 35px 5px 0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 28px;
+    color: #fff;
+  }
 </style>
--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,35 +1,32 @@
 <script></script>
 
 <div class="modal-wrapper">
-    <div class="modal-background"></div>
-        <div class="modal">
-        </div>
-
+  <div class="modal-background" />
+  <div class="modal" />
 </div>
 
 <style>
-    .modal-wrapper {
-        height: 100%;
-        width: 100%;
-        position: fixed;
-        z-index: 30;
-    }
+  .modal-wrapper {
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    z-index: 30;
+  }
 
-    .modal-background {
-        background: #c7daf3;
-        opacity: 0.1;
-        width: 100%;
-        height: 100%;
+  .modal-background {
+    background: #c7daf3;
+    opacity: 0.1;
+    width: 100%;
+    height: 100%;
+  }
 
-    }
-
-    .modal {
-        top: 10%;
-        left: 10%;
-        height: 80%;
-        width: 80%;
-        position: absolute;
-        z-index: 10;
-        background: white;
-    }
+  .modal {
+    top: 10%;
+    left: 10%;
+    height: 80%;
+    width: 80%;
+    position: absolute;
+    z-index: 10;
+    background: white;
+  }
 </style>
